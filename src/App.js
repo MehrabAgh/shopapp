cc40023e88ea3018bab3e948dcec02b65e8c5bae
mehrab aghaee <aghaeemehrab@gmail.com>
2022-03-26 18:09:06 +0430

    __test_01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div >
+        <
+        h2 className = 'xx' > Hello React { name } < /h2> { testCond } <
+        ul >
+        <
+        renderList > < /renderList> <
+        /ul> <
+        /div>    
+    );
 }
 
-export default App;
+const name = < em > Mehrab Aghaee < /em>
+let x = 10;
+let testCond = < strong > { x < 1 ? 'Good' : 'Bad' } < /strong>
+
+const testList = ['m', 'e', 'h', 'r', 'a', 'b'];
+
+const renderList = () => {
+        testList.map((n) => {
+                    return <li > { n } < /li>})
+                }
+
+
+                export default App;
