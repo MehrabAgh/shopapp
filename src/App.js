8b0fe335b073fbef5474cbb73eb48d785ca16d03
mehrab aghaee <aghaeemehrab@gmail.com>
2022-03-27 14:37:08 +0430

    __test_02

--- src/App.js
+++ src/App.js
@@ -1,28 +1,89 @@
+import React from 'react';
 import './App.css';
-
+import Testclass from './OtherPage/testClass';
 function App() {
-    return ( <
-        div >
-        <
-        h2 className = 'xx' > Hello React { name } < /h2> { testCond } <
-        ul >
-        <
-        renderList > < /renderList> <
-        /ul> <
-        /div>    
-    );
+  return (
+    <div>
+      <h2 className='xx'>Hello React {name}</h2>
+      {testCond}
+      <ul>
+        <Test use="Hello Props"/>
+      </ul>
+      <TimeShow />
+      <Testclass color="blue"/>
+      <button onClick={()=> pp("data printed")}>Print</button>
+      <Statment enable = {false} />
+    </div>
+  );
+}
+//#region Start_Components
+const name = <em>Mehrab Aghaee</em>
+let x = 10;
+let testCond = <strong>{x < 1 ? 'Good' : 'Bad'}</strong>
+
+//
+
+const TimeShow = () => {
+  let t = new Date().toTimeString();
+  return(
+    <p>{t}</p>
+  )
 }
 
-const name = < em > Mehrab Aghaee < /em>
-let x = 10;
-let testCond = < strong > { x < 1 ? 'Good' : 'Bad' } < /strong>
+//
+
+const pp = (n) =>{
+  console.log(n)
+}
+
+//
+
+
+const Statment = (props) =>{
+
+  const StatmentV2 = () =>{
+    return(
+      <>
+      {props.enable && 
+      <h1>
+        Enable State 2 
+      </h1>
+      }
+      
+      </>
+    )
+  }
+  if(props.enable){
+    return(
+      <>
+       <StatmentV2/>
+       <b>enable</b>
+       </>
+    )
+
+  }else{
+    return <b>disable</b>
+    
+  }
+
+}
+
+//
 
 const testList = ['m', 'e', 'h', 'r', 'a', 'b'];
 
-const renderList = () => {
-        testList.map((n) => {
-                    return <li > { n } < /li>})
-                }
+const RenderList = () => {
+  return testList.map((n) => <li>{n}</li>)
+}
+function Test(props) {
+  return (
+    <>
+    <h1>{props.use}</h1>
+    <RenderList></RenderList>
+    </>
+  )
+}
 
-
-                export default App;
+//
+//#endregion
+export default App;
---
+++ src/OtherPage/testClass.js
@@ -0,0 +1,44 @@
+import React from 'react';
+class testClass extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: 'red',
+            number_1 : 0,
+            number_2 : 0,
+            result : 0
+        };
+    }
+    changeState = () => {
+        this.setState({ color: 'white' })
+    }
+    calculate = () => {
+        this.setState({ result : 10 + 10})
+    }
+
+    componentDidMount(){
+        console.log("start")
+    }
+    componentDidUpdate(){        
+        setInterval(()=>{    
+            console.log("Update")        
+        },1000)
+    }
+    componentWillUnmount(){
+        console.log('End')
+    }  
+    render() {
+        return (
+            <>
+                <h3>hello Class Component</h3>
+                <summary> Use State :  <button onClick={this.changeState}> color </button> {this.state.color}</summary>
+                <summary> Use Prop : color =  {this.props.color}</summary>
+                <div>
+                    <button onClick={this.calculate}>Show</button>
+                    <h2>{this.state.result}</h2>
+                </div>
+            </>
+        )
+    }
+}
+export default testClass;
